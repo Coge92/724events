2c0fe69410e4184955335271573d6723a8163156
Coge92 <mh.melting@gmail.com>
2024-05-28 17:22:28 +0200

    First Commit - Correction bugs sur slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,11 +8,11 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0), // Ajout de "-1 " à byDateDesc.length l'index max ne peut pas etre égale à la longueur (index n°1 = 0)
       5000
     );
   };
@@ -45,7 +45,7 @@ const Slider = () => {
                   key={`${event.id}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={radioIdx === index} // Modification de la variable "idx" par "index" = state pour que le bouton radio soit checked
                 />
               ))}
             </div>
